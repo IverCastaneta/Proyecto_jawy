92b108046bef620afa676d044a97668f9ff59f09
Sebaxyz12 <sebastiancuentasmolina@gmail.com>
2026-03-25 16:20:05 -0400

    Solución al bug de dueño de lugar

--- src/app/pages/seleccion-rol/seleccion-rol.page.ts
+++ src/app/pages/seleccion-rol/seleccion-rol.page.ts
@@ -8,27 +8,28 @@ import { Router } from '@angular/router';
 })
 export class SeleccionRolPage implements OnInit {
 
-  rolSeleccionado: number | null = null;
+  rolSeleccionado: string | null = null;
 
+  // Los 4 roles oficiales con sus IDs en texto para la base de datos
   roles = [
-    { id: 1, nombre: 'Músico', imagenUrl: 'assets/img/roles/musico.svg' },
-    { id: 2, nombre: 'Banda', imagenUrl: 'assets/img/roles/banda.svg' },
-    { id: 3, nombre: 'Dueño de lugar', imagenUrl: 'assets/img/roles/dueno_lugar.svg' },
-    { id: 4, nombre: 'Productor', imagenUrl: 'assets/img/roles/productor.svg' },
+    { id: 'musico', nombre: 'Músico', imagenUrl: 'assets/img/roles/musico.svg' },
+    { id: 'banda', nombre: 'Banda', imagenUrl: 'assets/img/roles/banda.svg' },
+    { id: 'dueno', nombre: 'Dueño de lugar', imagenUrl: 'assets/img/roles/dueno_lugar.svg' },
+    { id: 'productor', nombre: 'Productor', imagenUrl: 'assets/img/roles/productor.svg' }
   ];
 
   constructor(private router: Router) { }
 
   ngOnInit() { }
 
-  seleccionar(id: number) {
+  seleccionar(id: string) {
     this.rolSeleccionado = id;
   }
 
   siguiente() {
     if (this.rolSeleccionado !== null) {
       const registroInicial = {
-        rol: this.rolSeleccionado
+        rol: this.rolSeleccionado 
       };
 
       localStorage.setItem('temp_registro', JSON.stringify(registroInicial));
